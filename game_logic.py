ec860eeff789eb45a03a5dda0fa75a50e21857b9
BatTrackr <49361303+BatTrackr@users.noreply.github.com>
2019-12-21 10:02:56 +0000

    Add files via upload
    Incomplete skeleton game logic

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,20 @@
-class GameLogic:
+class GameLogic(object):
     print("Game Logic Object Created")
-    def
+    # TODO add code here to manage the logic of your game
+    def __init__(self, board, piece):
+        liberties = []
+        self.board = board
+        self.stone = [piece]
 
+        def remove(self):
+
+            while self.stone:
+                self.stone[0].remove()
+
+            self.board.stone.remove()
+
+        def rules(self):
+            #if a move would enforce 0 liberties on a piece, it is invalid
+            if 0 in liberties:
+                self.remove()
 
