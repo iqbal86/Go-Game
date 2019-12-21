118640918da52256e70d0fa709c55208bc4b90c1
BatTrackr <49361303+BatTrackr@users.noreply.github.com>
2019-12-21 18:32:29 +0000

    Add files via upload

--- game_logic.py
+++ game_logic.py
@@ -6,15 +6,52 @@ class GameLogic(object):
         self.board = board
         self.stone = [piece]
 
+    def makeMove(self, col, row):
+            '''tries to move'''
+            if self.isEmpty(col, row):  # check if the space is empty
+                if self.notKO(col, row):  # checks to make sure it is not a KO
+                    self.createListofPlayerLocationsAroundCurrentMoveAndLibertyCount(col,
+                                                                                     row)  # call specified function, explained below
+                    self.create4ListsOfOppoentLocationsAroundCurrentMoveLocationAnd4LibertyCounts(col,
+                                                                                                  row)  # call specified function, explained below
+                    self.capture()  # capture pieces
+                    if self.notSuicide(col, row):  # check to see if it is a Suicide move
+                        self.placePiece(col, row)  # place the peice
+                        self.updateScoreBoard()  # update the score board
+                        self.checkWinner()  # see if there is a winner
+                        self.switchPlayer()  # switch the player
+                        self.update()  # referesh the widget
+
+
         def remove(self):
 
             while self.stone:
                 self.stone[0].remove()
 
-            self.board.stone.remove()
 
-        def rules(self):
-            #if a move would enforce 0 liberties on a piece, it is invalid
-            if 0 in liberties:
-                self.remove()
 
+        def withinBoardRange(self, col, row):
+            ''' Check to see if the col and row are within the board number of columns and rows
+                    returns true if they are
+                    returns false if they are not'''
+            if row in self.board.row:
+                if col in self.board.col:
+                    return True
+                else:
+                    return False
+
+        def notInLocationList(self, list, col, row):
+                '''returns true if the col and row is not in the list'''
+                if col not in list:
+                    if row not in list:
+                        return True
+                    else:
+                        return False
+
+        def isEmpty(self, col, row):
+            '''Checks to see if the location specified by the row and col empty'''
+            if 0 in col:
+                if 0 in row:
+                    return True
+            else:
+                return False
